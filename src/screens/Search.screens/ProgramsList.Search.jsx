269c8b5d2f1c50f9389211f8a3888cc9b54f48b4
shashi-iflextech <shashi@iflextech.in>
2025-08-26 11:30:08 +0530

    search filter fixes done

--- src/screens/Search.screens/ProgramsList.Search.jsx
+++ src/screens/Search.screens/ProgramsList.Search.jsx
@@ -2,25 +2,48 @@ import React from "react";
 import { X } from "lucide-react";
 import ProgramCard from "./programCard.Search";
 
-export default function programList({ programCards }) {
+export default function programList({ programCards, setfilteredProgramsList }) {
+
   const shortBy = [
-    "Newly published",
-    "Oldest published",
-    "Alphabetical",
-    "Price: Low to High",
-    "Price: High to Low",
+    // "Newly published",
+    // "Oldest published",
+    // "Alphabetical",
+    "Low to High",
+    "High to Low",
   ];
+
+  // Convert application_fees safely
+  const getFee = (fee) => {
+    if (fee === null || fee === "null") return 0; // treat null as 0
+    return Number(fee); // convert string to number
+  };
+
+  function sortPrograms(val) {
+    if (val == 'Low to High') {
+      const ascending = [...programCards].sort((a, b) => getFee(a.application_fees) - getFee(b.application_fees));
+      setfilteredProgramsList(ascending);
+    }
+    else {
+      const descending = [...programCards].sort((a, b) => getFee(b.application_fees) - getFee(a.application_fees));
+      setfilteredProgramsList(descending);
+    }
+  }
+
   return (
     <>
       <div className="space-y-4">
         <div className="h-12 flex justify-between items-center">
-          <div className="rounded-full bg-gray-200 text-gray-500 border px-3 py-1 flex justify-between items-center gap-2">
+          {/* <div className="rounded-full bg-gray-200 text-gray-500 border px-3 py-1 flex justify-between items-center gap-2">
             <p className="text-sm ">filter search</p>
             <X className="w-3" />
-          </div>
+          </div> */}
+          <div className=""></div>
           <div className="flex items-center gap-2">
             <label htmlFor="">Short By:</label>
-            <select className="h-12 w-[191px] px-1 rounded bg-gray-50 border">
+            <select className="h-12 w-[191px] px-1 rounded bg-gray-50 border" onChange={(e) => sortPrograms(e.target.value)}>
+              <option selected disabled>
+                Fees
+              </option>
               {shortBy.map((item) => (
                 <option value={item} className="">
                   {item}
